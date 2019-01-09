ad5693e635b4e0f9af06250f092e4d811cd205b0
Estayparadox <pereniguez.joseph@hotmail.fr>
2019-01-09 10:32:30 +0800

    [-] pods

--- Tek2/Piscine-Swift/Epispy/Epispy/Pods/Target Support Files/Pods-Epispy 2.0/Pods-Epispy 2.0-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Epispy_2_0VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Epispy_2_0VersionString[];
-
--- Tek2/Piscine-Swift/Epispy/Epispy/Pods/Target Support Files/SlideMenuControllerSwift/SlideMenuControllerSwift-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SlideMenuControllerSwift.h"
-
-FOUNDATION_EXPORT double SlideMenuControllerSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char SlideMenuControllerSwiftVersionString[];
-
